b8cd3b3d804544ccdb56538492f8998c601c5854
armand <armandcoiffe@gmail.com>
2025-11-24 22:34:44 +0100

    testing additionnal param, preparing for hyperparameter tuning, organisation of the code, updated timeline

--- training/optimizer.py
+++ training/optimizer.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def get_optimizer(model):
+def get_optimizer(model, lr=1e-4, weight_decay=1e-6):
     return torch.optim.Adam(
-        model.parameters(), lr=5e-4, weight_decay=1e-6
+        model.parameters(), lr=lr, weight_decay=weight_decay
     )  # lr and weight_decay to be tuned
