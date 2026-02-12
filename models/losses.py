06796feded0613a7c2336d4f056ad372bb60ca6f
armand <armandcoiffe@gmail.com>
2026-02-12 22:13:37 +0100

    minor edits and errors modification

--- models/losses.py
+++ models/losses.py
@@ -1,26 +1,40 @@
 import torch
-from utils.utils import reconstruct_cov
 import numpy as np
+from utils import reconstruct
 
 
-# Potters–Bouchaud loss for corr
-def loss_function_corr(Corr_oos, Corr_pred, T):
-    B, N, _ = Corr_oos.shape
+# Potters–Bouchaud loss
+def loss_function_mat(Mat_oos, Mat_pred, T):
+    B, N, _ = Mat_oos.shape
 
     # Matrix difference
-    Delta = Corr_pred - Corr_oos  # (B, N, N)
+    Delta = Mat_pred - Mat_oos  # (B, N, N)
 
     ## CB: It seems more efficient to compute the Frobenius norm via squaring element-wise and summing.
     # Square of the matrix (Delta^2 = Delta @ Delta) Symetric matrix so we don't need to transpose !
-    Delta2 = Delta @ Delta  # (B, N, N)
+    Delta2 = torch.transpose(Delta, 1, 2) @ Delta  # (B, N, N)
 
     # Trace of Delta2 = sum of diagonal
     trace_vals = Delta2.diagonal(dim1=1, dim2=2).sum(dim=1)  # (B,)
 
     # Normalized Frobenius estimation error (Potters-Bouchaud)
-    loss_cov = torch.sqrt(trace_vals) * np.sqrt(T) / N  # (B,)
+    loss_cov = torch.sqrt(trace_vals) * T / N**2  # (B,)
 
     return loss_cov.mean()  # scalar
+
+
+def log_spectrum_mse(Mat_oos, Mat_pred, T):
+    """
+    lam_pred:   (B, N) predicted eigenvalues (positive)
+    lam_target: (B, N) target eigenvalues (positive)
+    """
+    eps = 1e-6
+    lam_pred, _ = torch.linalg.eigh(Mat_oos)
+    log_pred = torch.log(lam_pred + eps)
+
+    lam_target, _ = torch.linalg.eigh(Mat_pred)
+    log_tgt = torch.log(lam_target + eps)
+    return ((log_pred - log_tgt) ** 2).mean()
 
 
 import tensorflow as tf
@@ -72,9 +86,10 @@ def loss_function(lam_pred, Q, Sigma_oos, T):
     return loss_cov.mean()  # scalar
 
 
+# NOT USED For NOW
 # loss function which is actually : -profit, beacause we want to maximize them
 def loss_function_portfolio(lam_pred, Q, R_oos, T):
-    Sigma = reconstruct_cov(Q, lam_pred)
+    Sigma = reconstruct(Q, lam_pred)
     B, N, _ = Sigma.shape
     ones = torch.ones(B, N, 1, dtype=Sigma.dtype)
     x = torch.linalg.solve(Sigma, ones)
--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def reconstruct_cov(Q, lam):
+def reconstruct(Q, lam):
     Lambda = torch.diag_embed(lam.squeeze(-1))
     Sigma = Q @ Lambda @ Q.transpose(1, 2)
     return Sigma
