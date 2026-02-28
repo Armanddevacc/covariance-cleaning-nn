86f235df28896dddfba43a61d7e4977cf177c436
armand <armandcoiffe@gmail.com>
2026-02-28 18:02:52 +0100

    new TF clean trainer

--- test.py
+++ test.py
@@ -2,10 +2,9 @@ import torch.nn as nn
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-from data.dataloader import data_generator
-from models.losses import loss_function_mat
-from models.gru_denoiser import BiGRUSpectralDenoiser
-from training.trainer import Trainer
+from data.dataloader import tf_data_generator
+from models.gru_denoiser import BiGRUSpectralDenoiserTensorFlow
+from training.trainer import Trainer_tf_remake
 
 
 mse = nn.MSELoss()
@@ -15,26 +14,19 @@ T_min = 30
 T_max = 70
 batch_size = 100
 ### Training loop — Generated Data
-model_generated_data = BiGRUSpectralDenoiser(hidden_size=96)  # hidden_size to be tunned
+model_generated_data = BiGRUSpectralDenoiserTensorFlow(
+    hidden_size=96
+)  # hidden_size to be tunned
 # lr, weight_decay, batch_size, epochs, hidden_size to be tunned
-trainer = Trainer(
-    model=model_generated_data,
-    is_train_on_real_data=False,
-    loss_function=loss_function_mat,
-    data_generator=data_generator,
-    lr=1e-4,
-    weight_decay=1e-5,
-    batch_size=batch_size,
+trainer = Trainer_tf_remake(
+    model_generated_data,
+    tf_data_generator,
+    batch_size,
     epochs=50,
+    missing_constant=2,
     N_min=N_min,
     N_max=N_max,
     T_min=T_min,
     T_max=T_max,
-    log_interval=10,
-    accumulate_steps=2,
-    dataset=None,
-    missing_constant=2,
 )
-# need to epochs to be big when there is lot of choise for N and T
-# need for big batch_size when df is big
-losses = trainer.train()
+losses = trainer.train(50)
--- training/trainer.py
+++ training/trainer.py
@@ -288,3 +288,99 @@ class Trainer_tf:
 
         print("Training complete.")
         return self.loss_history
+
+
+from tensorflow import keras
+from models.losses import tf_loss_function_mat
+
+
+class Trainer_tf_remake:
+    # gradientTpae : recuperer les gradients d'une fonctions de pertes par rapport a
+    # des poids trainable des couches
+    # utiliser ces gradients pour mettre à jours ces poids en les recuperant avec model.trainable_weights
+
+    def __init__(
+        self,
+        model,
+        data_generator,
+        batch_size,
+        epochs,
+        missing_constant,
+        N_min,
+        N_max,
+        T_min,
+        T_max,
+    ):
+        self.model = model
+        self.epochs = epochs
+        # We need parameters for the data generator
+        self.batch_size = batch_size
+        self.missing_constant = missing_constant
+        self.N_min = N_min
+        self.N_max = N_max
+        self.T_min = T_min
+        self.T_max = T_max
+        # first we need an optimizer :
+        self.optimizer = tf.keras.optimizers.Adam(1e-4)
+        # we need a loss function
+        self.loss_function = tf_loss_function_mat
+        # and a data generator
+        self.data_generator = data_generator(
+            self.batch_size,
+            self.missing_constant,
+            self.N_min,
+            self.N_max,
+            self.T_min,
+            self.T_max,
+        )
+        # save loss over time
+        self.loss_history = []
+
+    def train(self, steps_per_epoch):
+        for epoch in range(self.epochs):
+            epoch_loss = 0
+
+            for step in range(steps_per_epoch):
+                loss = self._train_step()
+                epoch_loss += loss
+            epoch_loss /= steps_per_epoch
+            self.loss_history.append(epoch_loss)
+
+            print(f"Epoch {epoch+1}, Loss: {epoch_loss:.6f}")
+        return self.loss_history
+
+    def _train_step(self):
+        input_seq, Q_emp, Sigma_true, T, Sigma_hat_diag, R_hat = next(
+            self.data_generator
+        )
+        Q_emp = tf.cast(Q_emp, tf.float32)
+        Sigma_hat_diag = tf.cast(Sigma_hat_diag, tf.float32)
+        Sigma_true = tf.cast(Sigma_true, tf.float32)
+
+        with tf.GradientTape() as tape:
+            # input_seq, containts Sigma_emp: (B, N, T)
+            lam_pred = self.model(input_seq, training=True)
+
+            # lam_pred: (B, N)
+            Sigma_pred = self._reconstruct_cov(lam_pred, Q_emp, Sigma_hat_diag)
+
+            loss = self.loss_function(Sigma_true, Sigma_pred, T)
+
+        grads = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+
+        return loss
+
+    def _reconstruct_cov(self, lam_pred, Q_emp, Sigma_hat_diag):
+
+        Sigma_pred = tf.matmul(
+            tf.matmul(
+                tf.matmul(
+                    tf.matmul(tf.sqrt(tf.linalg.diag(Sigma_hat_diag)), Q_emp),
+                    tf.linalg.diag(lam_pred),
+                ),
+                tf.transpose(Q_emp, perm=[0, 2, 1]),
+            ),
+            tf.sqrt(tf.linalg.diag(Sigma_hat_diag)),
+        )
+        return Sigma_pred
