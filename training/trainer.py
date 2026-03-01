d951aaaf1084898c25d99948d1fd388df6fe1c0f
armand <armandcoiffe@gmail.com>
2026-03-01 12:27:30 +0100

    Deleting former tf trainer + editing new trainer to add accumulated batch with different N and T

--- training/trainer.py
+++ training/trainer.py
@@ -114,188 +114,12 @@ class Trainer:
 
 
 import tensorflow as tf
-from estimator.MLE import tf_cov_pairwise
-
-
-class Trainer_tf:
-    def __init__(
-        self,
-        model,
-        is_train_on_real_data,
-        loss_function,
-        data_generator,
-        lr,
-        weight_decay,
-        batch_size,
-        epochs,
-        N_min,
-        N_max,
-        T_min,
-        T_max,
-        log_interval,
-        accumulate_steps,
-        dataset,
-        missing_constant,
-    ):
-        self.model = model
-        self.is_train_on_real_data = is_train_on_real_data
-        self.loss_function = loss_function
-        self.data_generator = data_generator
-        self.batch_size = batch_size
-        self.epochs = epochs
-        self.N_min = N_min
-        self.N_max = N_max
-        self.T_min = T_min
-        self.T_max = T_max
-        self.log_interval = log_interval
-        self.accumulate_steps = accumulate_steps
-        self.dataset = dataset
-        self.missing_constant = missing_constant
-
-        # Equivalent to get_optimizer(...)
-        self.optimizer = tf.keras.optimizers.AdamW(
-            learning_rate=lr, weight_decay=weight_decay
-        )
-
-        self.loss_history = []
-
-    def train(self):
-        print(f"Starting training for {self.epochs} epochs…")
-
-        if not self.is_train_on_real_data:
-            generator = self.data_generator(
-                self.batch_size,
-                N_min=self.N_min,
-                N_max=self.N_max,
-                T_min=self.T_min,
-                T_max=self.T_max,
-                missing_constant=self.missing_constant,
-            )
-        else:
-            generator = self.data_generator(
-                N_min=self.N_min,
-                N_max=self.N_max,
-                T_min=self.T_min,
-                T_max=self.T_max,
-                dataset=self.dataset,
-                missing_constant=self.missing_constant,
-            )
-
-        # --------- FORCE MODEL BUILD (VERY IMPORTANT) ----------
-        # Because in TF, variables are created lazily on first forward pass
-        input_seq, *_ = next(generator)
-        input_seq = tf.cast(input_seq, tf.float32)
-        _ = self.model(input_seq, training=True)
-
-        # Re-create generator because we consumed one batch just before
-        if not self.is_train_on_real_data:
-            generator = self.data_generator(
-                self.batch_size,
-                N_min=self.N_min,
-                N_max=self.N_max,
-                T_min=self.T_min,
-                T_max=self.T_max,
-                missing_constant=self.missing_constant,
-            )
-        else:
-            generator = self.data_generator(
-                N_min=self.N_min,
-                N_max=self.N_max,
-                T_min=self.T_min,
-                T_max=self.T_max,
-                dataset=self.dataset,
-                missing_constant=self.missing_constant,
-            )
-        # -------------------------------------------------------
-
-        for epoch in range(self.epochs):
-
-            vars_ = (
-                self.model.trainable_variables
-            )  # will compute gradients w.r.t. these variables
-            accum_grads = [tf.zeros_like(v) for v in vars_]
-
-            epoch_loss = 0
-
-            for _ in range(self.accumulate_steps):
-                input_seq, Q_emp, Mat_oos, T, Sigma_hat_diag, _ = next(generator)
-
-                # Convert numpy to tensors
-                input_seq = tf.cast(input_seq, dtype=tf.float32)
-                Q_emp = tf.cast(Q_emp, dtype=tf.float32)
-                Mat_oos = tf.cast(Mat_oos, dtype=tf.float32)
-                Sigma_hat_diag = tf.cast(Sigma_hat_diag, dtype=tf.float32)
-                T = tf.cast(T, tf.float32)
-
-                with tf.GradientTape() as tape:  # the tape will record operations for AAD (important to reinitiate it after each step of accumulation)
-
-                    # Forward through the network to estimate corr matrix eigenvalues
-                    # In the input_seq we pass :
-                    #   the eigenvalues of a corr matrix which has Q_emp*Sigma_hat_diag as transition matrix (to cov)
-                    #   N_vec, T_vec, which are sizes of the different matrix
-                    #   N_vec / T_vec, which is the ratio of ...
-                    #   Tmin_mean, Tmax_mean, which are featured engeneered and supposed to help with the prediction given though some variable are missing
-                    lam_pred = self.model(input_seq, training=True)
-
-                    D_sigma = tf.linalg.diag(Sigma_hat_diag)
-                    D_lam = tf.linalg.diag(lam_pred)
-
-                    cov_pred = tf.matmul(
-                        tf.matmul(
-                            tf.matmul(tf.matmul(D_sigma, Q_emp), D_lam),
-                            tf.transpose(Q_emp, perm=[0, 2, 1]),
-                        ),
-                        D_sigma,
-                    )  # compute loss on correlation
-
-                    # for later use
-                    # When we train on real data we don't have the real cov/corr matrix but we have oos data which we can use that way :
-                    if self.is_train_on_real_data:
-                        # in that case Mat_oos was oos return on 10 days; thanks to them we compute the covariance matrix of the assets
-                        # in the case where train_on_real_data is false the Mat_oos is already the true covariance matrix (we say true because it is thnak to it that return are generated)
-                        Mat_oos = tf_cov_pairwise(Mat_oos)
-
-                    # eps = 1e-12
-                    # diag_oos = tf.linalg.diag_part(Mat_oos)
-                    # std_oos = tf.sqrt(tf.maximum(diag_oos, eps))
-                    # corr_oos = Mat_oos / (std_oos[:, None, :] * std_oos[:, :, None] + eps)
-
-                    # at the end of the day we want the loss between covariance to be small so it is our loss criterion
-                    loss = self.loss_function(
-                        Mat_oos, cov_pred, T
-                    )  # call bouchaud loss
-                    loss_scaled = loss / float(self.accumulate_steps)
-
-                    epoch_loss += float(loss)
-
-                grads = tape.gradient(loss_scaled, vars_)
-                # handling of None gradients
-                accum_grads = [
-                    ag + (g if g is not None else tf.zeros_like(v))
-                    for ag, g, v in zip(accum_grads, grads, vars_)
-                ]
-
-            clipped_grads, _ = tf.clip_by_global_norm(accum_grads, 1.0)
-            self.optimizer.apply_gradients(zip(clipped_grads, vars_))
-
-            self.loss_history.append(epoch_loss / self.accumulate_steps)
-
-            # logging
-            if (epoch + 1) % self.log_interval == 0:
-                print(
-                    f"Epoch {epoch+1}/{self.epochs} — loss: {epoch_loss / self.accumulate_steps:.8f}"
-                )
-
-        print("Training complete.")
-        return self.loss_history
-
-
-from tensorflow import keras
+from estimator.MLE import tf_cov_pairwise  # to be used when out of sample data
 from models.losses import tf_loss_function_mat
 
 
-class Trainer_tf_remake:
-    # gradientTpae : recuperer les gradients d'une fonctions de pertes par rapport a
+class Trainer_tf:
+    # gradientTape : recuperer les gradients d'une fonctions de pertes par rapport a
     # des poids trainable des couches
     # utiliser ces gradients pour mettre à jours ces poids en les recuperant avec model.trainable_weights
 
@@ -337,34 +161,39 @@ class Trainer_tf_remake:
         self.loss_history = []
 
     def train(self, steps_per_epoch):
+        """
+        The training loop train over epoch * steps_per_epoch * batch number of
+        sample matrix
+        """
         for epoch in range(self.epochs):
-            epoch_loss = 0
 
-            for step in range(steps_per_epoch):
-                loss = self._train_step()
-                epoch_loss += loss
-            epoch_loss /= steps_per_epoch
-            self.loss_history.append(epoch_loss)
+            loss = self._train_step(steps_per_epoch)
 
-            print(f"Epoch {epoch+1}, Loss: {epoch_loss:.6f}")
+            self.loss_history.append(loss)
+
+            print(f"Epoch {epoch+1}, Loss: {loss:.6f}")
         return self.loss_history
 
-    def _train_step(self):
-        input_seq, Q_emp, Sigma_true, T, Sigma_hat_diag, R_hat = next(
-            self.data_generator
-        )
-        Q_emp = tf.cast(Q_emp, tf.float32)
-        Sigma_hat_diag = tf.cast(Sigma_hat_diag, tf.float32)
-        Sigma_true = tf.cast(Sigma_true, tf.float32)
+    def _train_step(self, steps_per_epoch):
+        """ """
 
         with tf.GradientTape() as tape:
-            # input_seq, containts Sigma_emp: (B, N, T)
-            lam_pred = self.model(input_seq, training=True)
+            loss = 0
+            for step in range(steps_per_epoch):
+                input_seq, Q_emp, Sigma_true, T, Sigma_hat_diag, R_hat = next(
+                    self.data_generator
+                )
+                Q_emp = tf.cast(Q_emp, tf.float32)
+                Sigma_hat_diag = tf.cast(Sigma_hat_diag, tf.float32)
+                Sigma_true = tf.cast(Sigma_true, tf.float32)
 
-            # lam_pred: (B, N)
-            Sigma_pred = self._reconstruct_cov(lam_pred, Q_emp, Sigma_hat_diag)
+                # input_seq, containts Sigma_emp: (B, N, T)
+                lam_pred = self.model(input_seq, training=True)
 
-            loss = self.loss_function(Sigma_true, Sigma_pred, T)
+                # lam_pred: (B, N)
+                Sigma_pred = self._reconstruct_cov(lam_pred, Q_emp, Sigma_hat_diag)
+
+                loss += self.loss_function(Sigma_true, Sigma_pred, T) / steps_per_epoch
 
         grads = tape.gradient(loss, self.model.trainable_variables)
         self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
